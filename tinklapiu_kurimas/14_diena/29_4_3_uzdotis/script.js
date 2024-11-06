1d83af74aacf4e6478354686723d3a4a2ff6caed
D2JP <d2jp@TECHIN.INTERNAL>
2024-11-06 09:20:19 +0400

    Complite 29_4_3_uzdotis

--- tinklapiu_kurimas/14_diena/29_4_3_uzdotis/script.js
+++ tinklapiu_kurimas/14_diena/29_4_3_uzdotis/script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+const saldainiai = +prompt("Kiek saldeniu gavo Petriukas"); //25;
+
+let saldainiiuLiko;
+let likoSaldainiiu = saldainiai;
+let diena = 0;
+while (saldainiai > 0) {
+  const saldainiuSuvage = +prompt("Kiek saldeiniu suvalge per diena");
+  likoSaldainiiu = likoSaldainiiu - saldainiuSuvage;
+  if (likoSaldainiiu > 0) {
+    saldainiiuLiko = likoSaldainiiu;
+    diena++;
+  } else if (likoSaldainiiu <= 0) {
+    break;
+  }
+}
+console.log(
+  `Petriukas saidainiu uzteks ${diena} ir jam liks ${saldainiiuLiko} saldainiai.`
+);
