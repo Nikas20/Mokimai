76802f28574dc813b5f8a1c600c3c9c7269cf64d
D2JP <d2jp@TECHIN.INTERNAL>
2024-11-13 11:34:30 +0400

    Create react folder, Complite 2uzdotis

---
+++ react/2uzdotis/script.js
@@ -0,0 +1,31 @@
+function calculate(num1, num2, operation){
+    try{
+        if(operation === "+"){
+            const result = num1 + num2
+        return result;
+        }
+        else if(operation === "-"){
+            const result = num1 - num2
+        return result;   
+        }
+        else if(operation === "*"){
+            const result = num1 * num2
+        return result;    
+        }
+        else if(operation === "/" && num1 !== 0 && num2 !==0){
+            const result = num1 / num2
+        return result;    
+        }
+        else if(operation === "/" && num1 === 0 || num2 ===0){
+            throw new Error("Cannot divide by zero")
+        }
+        else if(operation !== "+" && operation !== "-" && operation !== "*" && operation !== "/"){
+            throw new Error("Invalid operation")
+        }
+    } 
+    catch(error){console.error(error);}
+}
+console.log(calculate(10, 0, "+"));
+console.log(calculate(10, 2, "/"));
+//console.log(calculate(10, 0, "/"));
+//console.log(calculate(10, 2, "&"));
