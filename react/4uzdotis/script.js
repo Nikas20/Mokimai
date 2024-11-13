81ae3e920afbb2a61d60f618ac6461573d4805f8
D2JP <d2jp@TECHIN.INTERNAL>
2024-11-13 14:43:30 +0400

    Modefy 4uzdotis

--- react/4uzdotis/script.js
+++ react/4uzdotis/script.js
@@ -1,19 +1,20 @@
 async function displayUserName(userId) {
+  const divdata = document.querySelector("#user-info");
   try {
     const response = await fetch(
       `https://jsonplaceholder.typicode.com/users/${userId}`
     );
+
     if (response.status === 404) {
-        const divdata = document.querySelector("#user-info")
-      divdata.textContent = "User not found"
+      divdata.textContent = "User not found";
       throw new Error("User not found");
     } else {
       const data = await response.json();
-      const divdata = document.querySelector("#user-info")
-      divdata.textContent = data.name
+
+      divdata.textContent = data.name;
     }
   } catch (error) {
     console.error(error);
   }
 }
-displayUserName(1);
+displayUserName(11);
