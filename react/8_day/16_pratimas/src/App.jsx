cb9642c54668dcd3cad0a9e2bc4692796d7cdd89
D2JP <d2jp@TECHIN.INTERNAL>
2024-11-20 13:19:52 +0400

    Complite 16_patimas

---
+++ react/8_day/16_pratimas/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import PostList from './components/PostList'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <PostList/>
+    </div>
+  );
+}
+
+export default App
