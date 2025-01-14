87a510927a4e1cd05a049c0a59d95cd135d9f0e0
Nikas <d2jp@TECHIN.INTERNAL>
2025-01-14 12:05:15 +0200

    Add AL_10

---
+++ java/day 6/Array list/AL_10.IndexOf/src/main/java/IndexOf.java
@@ -0,0 +1,31 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class IndexOf {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == -1) {
+                break;
+            }
+
+            list.add(input);
+        }
+
+        System.out.println("");
+
+        // implement here finding the indices of a number
+        System.out.printf("Search for? ");
+        int find = Integer.valueOf(scanner.nextLine());
+        for (int i = 0; i < list.size(); i++) {
+            if (find == list.get(i)) {
+                System.out.println(find + " is at index " + i);
+            }
+        }
+    }
+}
