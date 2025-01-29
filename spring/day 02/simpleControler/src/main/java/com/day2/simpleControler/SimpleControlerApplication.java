161d5a1afcff6b354d5d280c887bf263ff81e82a
Nikas <d2jp@TECHIN.INTERNAL>
2025-01-29 11:10:00 +0200

    Add simpleContlorer

---
+++ spring/day 02/simpleControler/src/main/java/com/day2/simpleControler/SimpleControlerApplication.java
@@ -0,0 +1,13 @@
+package com.day2.simpleControler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleControlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleControlerApplication.class, args);
+	}
+
+}
---
+++ spring/day 02/simpleControler/src/test/java/com/day2/simpleControler/SimpleControlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.day2.simpleControler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleControlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
