61e494039226c3bffb8761c34ac058372431e182
Nikas <d2jp@TECHIN.INTERNAL>
2025-02-04 13:14:26 +0200

    Add 5_2_uzdotis

---
+++ spring/day 05/demo - Copy - Copy/src/main/java/lt/techin/demo/controler/MovieController.java
@@ -0,0 +1,82 @@
+package lt.techin.demo.controler;
+
+import jakarta.validation.Valid;
+import lt.techin.demo.model.Movie;
+import lt.techin.demo.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class MovieController {
+
+  private final MovieService movieService;
+
+  @Autowired
+  public MovieController(MovieService movieService) {
+    this.movieService = movieService;
+  }
+
+  @GetMapping("/movies")
+  public ResponseEntity<List<Movie>> getMovies() {
+    return ResponseEntity.ok(movieService.findAllMovies());
+  }
+
+  @GetMapping("/movies/{id}")
+  public ResponseEntity<Movie> getMovie(@PathVariable long id) {
+    Optional<Movie> movieFind = movieService.findMovie(id);
+
+    if (movieFind.isEmpty()) {
+      return ResponseEntity.notFound().build();
+    }
+    return ResponseEntity.ok(movieFind.get());
+  }
+
+  @PostMapping("/movies")
+  public ResponseEntity<?> saveMovie(@Valid @RequestBody Movie movie) {
+    Movie mov = movieService.saveMuvie(movie);
+
+    return ResponseEntity.created(
+                    ServletUriComponentsBuilder.fromCurrentRequest()
+                            .path("/{id}")
+                            .buildAndExpand(mov.getId())
+                            .toUri())
+            .body(movie);
+  }
+
+  @PutMapping("/movie/{id}")
+  public ResponseEntity<?> updateMovie(@PathVariable long id, @Valid @RequestBody Movie movie) {
+
+    if (movieService.existsMovieById(id)) {
+      Movie movieF = movieService.findMovie(id).get();
+
+      movieF.setTitle(movie.getTitle());
+      movieF.setDirector(movie.getDirector());
+
+      return ResponseEntity.ok((movieService.saveMuvie(movieF)));
+    }
+    Movie savedMovie = movieService.saveMuvie(movie);
+
+    return ResponseEntity.created(
+                    ServletUriComponentsBuilder.fromCurrentRequest()
+                            .replacePath("/movie/{id}")
+                            .buildAndExpand(savedMovie.getId())
+                            .toUri())
+            .body(movie);
+  }
+
+  @DeleteMapping("/movie/{id}")
+  public ResponseEntity<?> deleteMovie(@PathVariable long id) {
+
+    if (!movieService.existsMovieById(id)) {
+      return ResponseEntity.notFound().build();
+    }
+    movieService.deleteMovieById(id);
+    return ResponseEntity.noContent().build();
+  }
+}
