4ca038bcb05c732746162f88b18629d470b6aeea
Nikas <simatolskisnikas@gmail.com>
2025-05-19 14:31:14 +0300

    modif

--- projekt/api/src/main/java/com/api/model/Account.java
+++ projekt/api/src/main/java/com/api/model/Account.java
@@ -12,69 +12,70 @@ import java.util.List;
 @Table(name = "users")
 public class Account implements UserDetails {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
-  private String email;
-  private String password;
+    private String email;
+    private String password;
 
-  @ManyToMany(fetch = FetchType.EAGER)
-  @JoinTable(
-          name = "account_roles",
-          joinColumns = @JoinColumn(name = "account_id"),
-          inverseJoinColumns = @JoinColumn(name = "role_id")
-  )
-  private List<Role> roles = new ArrayList<>();
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private List<Role> roles = new ArrayList<>();
 
-  public Account(String email, String password, List<Role> roles) {
-    this.email = email;
-    this.password = password;
-    this.roles = roles;
-  }
+    public Account(String email, String password, List<Role> roles) {
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
 
-  public Account() {
-  }
+    public Account() {
+    }
 
-  public long getId() {
-    return id;
-  }
+    public long getId() {
+        return id;
+    }
 
-  public void setId(long id) {
-    this.id = id;
-  }
+    public void setId(long id) {
+        this.id = id;
+    }
 
-  public String getEmail() {
-    return email;
-  }
+    public String getEmail() {
+        return email;
+    }
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-  @Override
-  public String getPassword() {
-    return password;
-  }
+    @Override
+    public String getPassword() {
+        return password;
+    }
 
-  public void setPassword(String password) {
-    this.password = password;
-  }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-  public List<Role> getRoles() {
-    return roles;
-  }
+    public List<Role> getRoles() {
+        return roles;
+    }
 
-  public void setRoles(List<Role> roles) {
-    this.roles = roles;
-  }
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
 
-  @Override
-  public Collection<? extends GrantedAuthority> getAuthorities() {
-    return roles;
-  }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
 
-  @Override
-  public String getUsername() {
-    return this.email;
-  }}
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+}
--- projekt/api/src/main/java/com/api/security/SecurityConfig.java
+++ projekt/api/src/main/java/com/api/security/SecurityConfig.java
@@ -4,29 +4,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
-import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableMethodSecurity
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(c -> c.disable())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/api/account").permitAll()
                         .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                );
+
 
         return http.build();
     }
