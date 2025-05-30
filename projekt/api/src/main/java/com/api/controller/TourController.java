ec52fcabd9a3ec48eb3756e975d0f89c86c01925
Nikas <simatolskisnikas@gmail.com>
2025-05-30 11:11:12 +0300

    modif

--- projekt/api/src/main/java/com/api/controller/TourController.java
+++ projekt/api/src/main/java/com/api/controller/TourController.java
@@ -1,33 +1,53 @@
 package com.api.controller;
 
+import com.api.dto.TourDTO.TourMapper;
+import com.api.dto.TourDTO.TourResponseDTO;
+import com.api.dto.TourDTO.ToutRequiestDTO;
 import com.api.model.Tour;
 import com.api.service.TourService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class TourController {
+    private final TourService tourService;
 
-  private final TourService tourService;
-
-  public TourController(TourService tourService) {
-    this.tourService = tourService;
-  }
-
-  @GetMapping("/tuor")
-  public ResponseEntity<?> getAllServices(){
-
-    List<Tour> allTuors = tourService.findAllTours();
-
-    if(allTuors.isEmpty()){
-      return ResponseEntity.noContent().build();
+    public TourController(TourService tourService) {
+        this.tourService = tourService;
     }
 
-    return ResponseEntity.ok().build();
-  }
+    @GetMapping("/tuor")
+    public ResponseEntity<?> getAllServices() {
+        List<Tour> allTuors = tourService.findAllTours();
+        if (allTuors.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(TourMapper.toTourListDTO(allTuors));
+    }
+
+    @PostMapping("/tour")
+    public ResponseEntity<?> addTour(@Valid @RequestBody ToutRequiestDTO toutRequiestDTO) {
+        Tour tour = new Tour();
+        tour.setTitle(toutRequiestDTO.title());
+        tour.setDescription(toutRequiestDTO.description());
+        tour.setPhoto_url(toutRequiestDTO.photo_url());
+        tour.setDuration_minutes(toutRequiestDTO.duration_minutes());
+        tour.setPrice(toutRequiestDTO.price());
+        TourResponseDTO newTour = TourMapper.toTourDTO(tourService.saveTour(tour));
+        return ResponseEntity.status(HttpStatus.CREATED).body(newTour);
+    }
+
+    @DeleteMapping("/tour/{id}")
+    public ResponseEntity<?> deleteTour(@PathVariable long id) {
+        if (!tourService.existByID(id)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Tour not found");
+        }
+        tourService.deleteTourById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- projekt/api/src/main/java/com/api/service/TourService.java
+++ projekt/api/src/main/java/com/api/service/TourService.java
@@ -1,5 +1,6 @@
 package com.api.service;
 
+import com.api.model.Account;
 import com.api.model.Tour;
 import com.api.repository.TourRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,16 +12,27 @@ import java.util.List;
 
 @Service
 public class TourService {
-  private final TourRepository tourRepository;
+    private final TourRepository tourRepository;
 
-  @Autowired
+    @Autowired
 
-  public TourService(TourRepository tourRepository) {
-    this.tourRepository = tourRepository;
-  }
+    public TourService(TourRepository tourRepository) {
+        this.tourRepository = tourRepository;
+    }
 
-  public List<Tour> findAllTours(){
-    return tourRepository.findAll();
-  }
+    public List<Tour> findAllTours() {
+        return tourRepository.findAll();
+    }
 
+    public Tour saveTour(Tour tour) {
+        return tourRepository.save(tour);
+    }
+
+    public boolean existByID(long id) {
+        return tourRepository.existsById(id);
+    }
+
+    public void deleteTourById(long id) {
+        tourRepository.deleteById(id);
+    }
 }
